52a9680d194b5bd2d3435422ad4dab5cba6261f3
katyagpe <salas-rodriguez@hotmail.com>
2018-07-29 00:14:42 -0500

    archivos de raiz

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php
+include('include/header.php');
+?>
+<style type="text/css">
+	a.email { color: #ffffff; }
+	.email:hover  { color: #CEE3F6 !important;  }
+</style>
+	<div class="slider">
+		<div class="img-responsive">
+			<ul class="bxslider">				
+				<li><img src="<?=PATH?>images/slider/web.png" alt=""/></li>								
+				<li><img src="<?=PATH?>images/slider/web3.png" alt=""/></li>	
+				<li><img src="<?=PATH?>images/slider/web4.png" alt=""/></li>			
+			</ul>
+		</div>	
+    </div>
+	
+	<div class="container">
+		<div class="text-center">
+			<div class="wow bounceInDown text_nosotros" data-wow-offset="0" data-wow-delay="0.3s">
+				<h3>Diseños web</h3>
+			</div>
+			<div class="wow bounceInDown text_nosotros" data-wow-offset="0" data-wow-delay="0.6s">
+				<h2>Soluciones creativas</h2>
+			</div>
+		</div>
+	</div>
+	
+		<div class="box">
+			<div class="wow fadeInLeft" data-wow-offset="0" data-wow-delay="0.4s">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<!--<i class="fa fa-cogs fa-3x"></i>-->
+					<i class="fa fa-tablet fa-3x" aria-hidden="true"></i>
+					<h4>Páginas responsivas</h4>
+				</div>
+			</div>
+			<div class="wow fadeInLeft" data-wow-offset="0" data-wow-delay="0.8s">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<!--<i class="fa fa-leaf fa-3x"></i>-->
+					<i class="fa fa-lock fa-3x" aria-hidden="true"></i>
+					<h4>estáticas</h4>
+				</div>
+			</div>
+			<div class="wow fadeInRight" data-wow-offset="0" data-wow-delay="1.2s">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<!--<i class="fa fa-heart-o fa-3x"></i>-->
+					<i class="fa fa-cog fa-3x" aria-hidden="true"></i>
+					<h4>Autoadminstrables</h4>
+				</div>
+			</div>
+			<div class="wow fadeInRight" data-wow-offset="0" data-wow-delay="1.6s">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<i class="fa fa-laptop fa-3x"></i>
+					<h4>Hosting y Dominio</h4>
+				</div>
+			</div>
+		</div>
+	
+	
+	
+	<div class="container">
+		
+		
+		<div class="col-lg-12 wow fadeInLeft" data-wow-offset="0" data-wow-delay="0.5s">
+			<img src="images/edicion.png" class="img-responsive imagen_edicion" >
+		</div>
+	</div>
+	
+	
+	
+	<div class="about-us">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="cuadro_team">
+						<h1>Equipo</h1>
+						<hr>
+					</div>
+				</div>
+
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 col-lg-offset-4 col-md-offset-4 col-sm-offset-4 wow fadeInLeft" data-wow-offset="0" data-wow-delay="0.2s">
+						<div class="cuadros" style="background-image: url('images/yo.jpg');background-size: cover;background-position: center;background-repeat: no-repeat;">
+							
+						</div>
+						<div class="informacion">
+							<h1>Katya Guadalupe Salas Rodríguez</h1>
+							<h2>Programador Web</h2>
+							<ul class="socials-connect">
+			                  <li><a title="" href="http://www.facebook.com/katy10salas" class="page_social"><i class="fa fa-facebook"></i></a></li>
+			                  <li><a class="email" href="mailto:salas-rodriguez@hotmail.com">salas-rodriguez@hotmail.com</a></li>
+			                  
+			                </ul>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<?php include('include/footer.php'); ?>
+
+<?php include('include/scripts.php'); ?>
+    
+</body>
+</html>

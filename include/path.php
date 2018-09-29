7790973d8a9fcc030337b94101808d226eba33a6
katyagpe <salas-rodriguez@hotmail.com>
2018-09-29 10:02:29 -0500

    eliminacion de archivos

--- include/path.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-	//para usarlo en un servidor se cambia.
-	//path para usar local
-	//define('PATH','http://localhost/agencia2/agencia/');
-	define('PATH','http://sitioisweb.16mb.com/');
-?>
